c5287ecbcfa3fc1c2d9052a896fb2ac893092760
migueljrm95 <miguelj.rodriguezmartinez@gmail.com>
2023-06-15 09:27:00 +0200

    chore: first commit

---
+++ src/main/java/com/migueljrm95/marvelservice/MarvelServiceApplication.java
@@ -0,0 +1,13 @@
+package com.migueljrm95.marvelservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvelServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarvelServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/migueljrm95/marvelservice/MarvelServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.migueljrm95.marvelservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarvelServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
